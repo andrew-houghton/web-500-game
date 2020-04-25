0f3de5ab1e25ead6b1b7921f5369a603f595c009
Andrew Houghton <houghtonandrew0@gmail.com>
2020-04-25 13:03:48 +1000

    Allow selecting kitty and your partner

--- static/canvas.js
+++ static/canvas.js
@@ -3,32 +3,35 @@ const cardHeight = 163;
 const perCardDegrees = 6;
 const perCardRadians = perCardDegrees * Math.PI / 180;
 const cardSpread = 300;
+var cardContainer = document.getElementById("cardContainer");
+
+function drawHand(cards, x, y, direction, playerId) {
+    var images = [];
+    for (let i = 0; i < cards.length; i++) {
+        images.push(new Image());
+        images[i].classList.add("cardPlayer"+playerId);
+        images[i].addEventListener('load', function() {
+            card_degrees = -cards.length * perCardDegrees / 2 + 3 + perCardDegrees * i + direction;
+            var cardX = Math.floor(x + Math.sin(card_degrees * Math.PI / 180) * cardSpread);
+            var cardY = Math.floor(y - Math.cos(card_degrees * Math.PI / 180) * cardSpread);
+            cardX = cardX - cardSpread / 3 * Math.sin(direction * Math.PI / 180);
+            cardY = cardY + cardSpread / 3 * Math.cos(direction * Math.PI / 180);
+            draw(images[i], cardX, cardY, card_degrees);
+        });
+        images[i].src = "data:image/png;base64," + cardData[cards[i]];
+        cardContainer.appendChild(images[i]);
+    }
+    return images
+}
+
+function draw(img, x, y, rotation) {
+    img.setAttribute("style", "transform: rotate(" + rotation + "deg)");
+    img.style.left = (x - cardWidth / 2) + 'px';
+    img.style.top = (y - cardHeight / 2) + 'px';
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    var cardContainer = document.getElementById("cardContainer");
-
-    function draw(img, x, y, rotation) {
-        img.setAttribute("style", "transform: rotate(" + rotation + "deg)");
-        img.style.left = (x - cardWidth / 2) + 'px';
-        img.style.top = (y - cardHeight / 2) + 'px';
-    }
-
-    function drawHand(cards, x, y, direction) {
-        var images = [];
-        for (let i = 0; i < cards.length; i++) {
-            images.push(new Image());
-            images[i].addEventListener('load', function() {
-                card_degrees = -cards.length * perCardDegrees / 2 + 3 + perCardDegrees * i + direction;
-                var cardX = Math.floor(x + Math.sin(card_degrees * Math.PI / 180) * cardSpread);
-                var cardY = Math.floor(y - Math.cos(card_degrees * Math.PI / 180) * cardSpread);
-                cardX = cardX - cardSpread / 3 * Math.sin(direction * Math.PI / 180);
-                cardY = cardY + cardSpread / 3 * Math.cos(direction * Math.PI / 180);
-                draw(images[i], cardX, cardY, card_degrees);
-            });
-            images[i].src = "data:image/png;base64," + cardData[cards[i]];
-            cardContainer.appendChild(images[i]);
-        }
-    }
-
+    // Sample data
     handLocations = [
         [0.5, 1, 0],
         [0, 0.5, 90],
@@ -50,23 +53,66 @@ document.addEventListener("DOMContentLoaded", function() {
         "6 Spades",
         "6 Hearts",
     ];
+
     for (let i = 0; i < handLocations.length; i++) {
         drawHand(
             hands[i],
             handLocations[i][0] * window.innerWidth,
             handLocations[i][1] * window.innerHeight,
             handLocations[i][2],
+            i,
         );
 
+        // Set bid text locations
         var bidTextElement = document.getElementById("player" + i + "Bid");
         x = handLocations[i][0] * window.innerWidth + cardSpread * 1.2 * Math.sin(handLocations[i][2] * Math.PI / 180);
         y = handLocations[i][1] * window.innerHeight - cardSpread * 1.2 * Math.cos(handLocations[i][2] * Math.PI / 180);
-        console.log(bidTextElement, x, y);
         if (handLocations[i][2] != 180 && handLocations[i][2] != 0){
             bidTextElement.setAttribute("style", "transform: rotate(" + handLocations[i][2] + "deg)");
         }
         bidTextElement.style.left = x + 'px';
         bidTextElement.style.top = y + 'px';
+
+        // Set bid text
         bidTextElement.innerHTML = recentBids[i];
     }
+
 });
+
+function kittyCardClicked(e) {
+    console.log(e)
+}
+
+function giveKitty() {
+    kitty = ['diamond_9', 'spade_10', 'heart_king']
+
+    // Update the hand
+    document.querySelectorAll('img.cardPlayer0').forEach(e => e.remove());
+    images = drawHand(
+        hands[0].concat(kitty),
+        handLocations[0][0] * window.innerWidth,
+        handLocations[0][1] * window.innerHeight,
+        handLocations[0][2],
+        0,
+    );
+
+    // Allow selecting and deselecting cards
+    for (let i = 0; i < images.length; i++) {
+        images[i].addEventListener('click', function() {
+            if (images[i].classList.contains("selected")) {
+                images[i].classList.remove("selected");
+            } else if (document.querySelectorAll('img.cardPlayer0.selected').length < 3) {
+                images[i].classList.add("selected");
+            }
+        });
+    }
+
+    // Allow selecting and delesecting opponents
+    for (let playerId = 1; playerId < 5; playerId++){
+        document.querySelectorAll('img.cardPlayer'+playerId).forEach(e => e.addEventListener('click', function() {
+            document.querySelectorAll('img.chosen').forEach(j => j.classList.remove("chosen"))
+            document.querySelectorAll('img.cardPlayer'+playerId).forEach(j => j.classList.add("chosen"))
+        }));
+
+    }
+}
