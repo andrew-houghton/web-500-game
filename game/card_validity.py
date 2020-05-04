35c79a6bf8496ea53e760a251451e878082cdb23
Andrew Houghton <houghtonandrew0@gmail.com>
2020-05-04 12:32:11 +1000

    Calculated validity for leading the joker, now to handle following suit

--- game/card_validity.py
+++ game/card_validity.py
@@ -18,23 +18,46 @@ def _get_card_suit(bid_suit, card):
     return card.split("_")[0][0]
 
 
-def _get_valid_joker_suits():
-    return {"h", "d", "c", "s"}
+def _get_valid_joker_suits(trick_card_history, bid_suit, cards):
+    void_suits = set()
+    for player_index, lead_index, trick_cards in trick_card_history:
+        if _get_card_suit(bid_suit, trick_cards[lead_index]) != _get_card_suit(bid_suit, trick_cards[player_index]):
+            void_suits.add(_get_card_suit(bid_suit, trick_cards[lead_index]))
+
+    def is_suit_in_hand(suit):
+        return any(_get_card_suit(bid_suit, card) == suit for card in cards if card != "joker")
+
+    def has_suit_been_played(suit):
+        return any(
+            _get_card_suit(bid_suit, trick_cards[player_index]) == suit
+            for player_index, _, trick_cards in trick_card_history
+        )
+
+    valid_suits = set()
+    for suit in "hdcs":
+        if suit not in void_suits and (not has_suit_been_played(suit) or not is_suit_in_hand(suit)):
+            valid_suits.add(suit)
+    return valid_suits
 
 
-def is_card_valid(trick_cards, trick_card_history, bid_suit, cards, index):  
+def is_card_valid(trick_cards, trick_card_history, bid_suit, cards, index):
     # Joker in no trumps played any time TODO fix this
     if cards[index] == "joker" and bid_suit == "n":
         # If leading then must be first or last of suit
         # If following then joker follows suit (must be first or last)
-        valid_suits = _get_valid_joker_suits()
-        if valid_suits:
-            return (True, valid_suits)
-        else:
+        valid_suits = _get_valid_joker_suits(trick_card_history, bid_suit, cards)
+
+        if not valid_suits:
             return (False, None)
 
+        if set(trick_cards) == {""}:
+            return (True, valid_suits)
+        else:
+            # TODO following suit
+            pass
+
     # If the player is leading they can play any card
-    if set(trick_cards) == {''}:
+    if set(trick_cards) == {""}:
         return (True, None)
 
     # Otherwise they must follow suit
@@ -71,13 +94,14 @@ def _get_card_number(bid_suit, card):
             return 15
     return 11
 
+
 def winning_card_index(trick_cards, bid_suit, lead_index):
     # Joker always wins
     for i in range(len(trick_cards)):
         if "joker" in trick_cards[i]:
             return i
 
-    if bid_suit != 'n':
+    if bid_suit != "n":
         trumps_played = [card for card in trick_cards if _get_card_suit(bid_suit, card) == bid_suit]
         if len(trumps_played) > 0:
             winning_card = max(trumps_played, key=lambda card: _get_card_number(bid_suit, card))
